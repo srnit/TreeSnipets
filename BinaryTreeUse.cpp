faa42c26717a6bf7308494ddd9db7c28b49175a7
sudhanshu raman <ramansudhanshu150@gmail.com>
2020-02-04 20:06:32 +0530

    Update BinaryTreeUse.cpp

--- BinaryTreeUse.cpp
+++ BinaryTreeUse.cpp
@@ -242,6 +242,18 @@ IsBSTReturn isBST2(BinaryTreeNode<int>* root) {
 	return output;
 }
 
+bool isBST3(BinaryTreeNode<int>* root, int min = INT_MIN, int max = INT_MAX) {
+	if (root == NULL) {
+		return true;
+	}
+	if (root->data < min || root->data > max) {
+		return false;
+	}
+	bool isLeftOk = isBST3(root->left, min, root->data - 1);
+	bool isRightOk = isBST3(root->right, root->data, max);
+	return isLeftOk && isRightOk;
+}
+
 // 1 2 3 4 5 6 7 -1 -1 -1 -1 8 9 -1 -1 -1 -1 -1 -1
 int main() {
 	BinaryTreeNode<int>* root = takeInputLevelWise();
